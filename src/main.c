282906c98996bab3d4bee613b7d2ae48624bb34d
Lucas Souza <lucasgabriel3490@gmail.com>
2023-12-27 22:44:35 -0300

    updated cpuinfo folder

--- src/main.c
+++ src/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "cpuinfo.h"
 int main(void){
+	reg_struct registers;
+	cpuid cpuinfo;
 	load_struct(&cpuinfo, &registers);
 
 	puts("************* BASIC CPUID INFO *************************************\n");
 	printf("Maximum Input Value for Basic CPUID Information:	%d\n", cpuinfo.basic_info.maximum_value);
 	printf("Vendor Identification String:				%s\n", (char *)&cpuinfo.basic_info.string);
 	printf("Stepping ID:						%d\n", cpuinfo.version_information.stepping_id);
-	printf("Model:							%d\n", cpuinfo.version_information.model);
+	printf("Model:							0x%x\n", cpuinfo.version_information.model);
 	printf("Family ID:						0x%x\n", cpuinfo.version_information.family_id);
+	printf("Processor Type:						%d\n", cpuinfo.version_information.processor_type);
+	printf("Extended Model ID:					%d\n", cpuinfo.version_information.extended_model_id);
+	printf("Extended Family ID:					%d\n", cpuinfo.version_information.processor_type);
 	return 0;
 }
